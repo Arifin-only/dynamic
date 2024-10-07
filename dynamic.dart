da94f0cebefb18e4edf6318e63e1a45493e73787
Arifin-only <arifin180104@gmail.com>
2024-10-07 12:23:46 +0800

    Create dynamic.dart

---
+++ dynamic.dart
@@ -0,0 +1,13 @@
+ void main(){
+   dynamic variabel = 20;
+   print(variabel);
+
+   variabel = true;
+   print(variabel);
+
+   variabel = 3.14;
+   print(variabel);
+
+   variabel = 'Alfi';
+   print(variabel);
+ }
